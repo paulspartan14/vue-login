7e55a69959bc163d2a5483f58bb93de3aa902ccc
paulspartan14 <e17081416@itmerida.edu.mx>
2021-01-27 14:56:56 -0600

    install axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+
 // use bootstrap
-vue.use(BootstrapVue)
-vue.use(IconsPlugin)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+// use axios
+Vue.use(VueAxios, axios)
+
+// use ccs bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
