41b70925471e92b5e0c96d4b0785416ad053b00c
paulspartan14 <e17081416@itmerida.edu.mx>
2021-01-27 14:53:36 -0600

    added bootstrap into my proyect vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+// use bootstrap
+vue.use(BootstrapVue)
+vue.use(IconsPlugin)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   router,
